0852ae111188b9ba8ea27cd4c7a6e955169692b6
Brannium <JoHab211@web.de>
2019-05-22 01:48:24 +0200

    added ping command functionality

--- commands/cmd_ping.py
+++ commands/cmd_ping.py
@@ -1,4 +1,6 @@
-
+import time
 
 def ex(message, invoke, args, client):
-    yield from client.send_message(message.channel, "Pong!")
+    response = yield from client.send_message(message.channel, ":hourglass_flowing_sand:")
+    time = (response.timestamp - message.timestamp).total_seconds() * 1000
+    yield from client.edit_message(response, new_content=':hourglass_flowing_sand:%sms' % round(time))
